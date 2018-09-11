c6843e201b9356f7688b1569559aef1edbf45980
Daniel Santiago <dspelaez@gmail.com>
2018-09-11 11:26:32 -0700

    First commit. Read row data working well

---
+++ bomm/__init__.py
@@ -0,0 +1,16 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# vim:fdm=marker
+#
+# =============================================================
+#  Copyright © 2018 Daniel Santiago <dpelaez@cicese.edu.mx>
+#  Distributed under terms of the GNU/GPL license.
+# =============================================================
+
+"""
+File:     __init__.py
+Created:  2018-06-13 17:05
+"""
+
+from .read_raw_data import ReadRawData
---
+++ setup.py
@@ -0,0 +1,26 @@
+#! /usr/bin/env python
+# -*- coding: utf-8 -*-
+# vim:fenc=utf-8
+# vim:fdm=marker
+#
+# =============================================================
+#  Copyright © 2018 Daniel Santiago <dpelaez@cicese.edu.mx>
+#  Distributed under terms of the GNU/GPL license.
+# =============================================================
+
+"""
+File:     setup.py
+Created:  2018-06-13 16:46
+"""
+
+from setuptools import setup
+
+setup(name = 'bomm',
+      version = '0.1',
+      description = 'Program to handle BOMM data',
+      url = '',
+      author = 'Daniel Santiago',
+      author_email = 'dspelaez@gmail.com',
+      license = 'GNU',
+      packages = ['bomm'],
+      zip_safe = False)
